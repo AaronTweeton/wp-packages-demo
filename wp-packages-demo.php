8bc3290a09821413bc766e1add2835fab4288288
AaronTweeton <aaron@aarontweeton.com>
2023-04-27 05:56:37 -0700

    Initial commit. Created plugin.

---
+++ wp-packages-demo.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * Plugin Name: WordPress Packages Demo
+ * Description: A plugin solely written to show how @wordpress packages can be used.
+ * Version: 0.1.0
+ * Author: Aaron Tweeton
+ * Author URI: https://aarontweeton.com
+ */
