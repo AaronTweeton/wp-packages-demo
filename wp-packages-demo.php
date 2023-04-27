68da66dc7a7fa47341ac645678d67cb39943c2a3
AaronTweeton <aaron@aarontweeton.com>
2023-04-27 06:05:50 -0700

    Added functions for creating menu page.

--- wp-packages-demo.php
+++ wp-packages-demo.php
@@ -7,3 +7,25 @@
  * Author: Aaron Tweeton
  * Author URI: https://aarontweeton.com
  */
+
+function wp_packages_demo_compose_menu_page() {
+?>
+    <div class="wrap">
+        <h1><?php echo esc_html(get_admin_page_title()); ?></h1>
+    </div>
+<?php
+}
+
+
+function wp_packages_demo_create_page() {
+    add_menu_page(
+        'WordPress Packages Demo',
+        'WP Packages',
+        'manage_options',
+        'wp-packages',
+        'wp_packages_demo_compose_menu_page',
+        'dashicons-carrot',
+    );
+}
+
+add_action('admin_menu', 'wp_packages_demo_create_page');
