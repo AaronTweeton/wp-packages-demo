8d3f03e691b37385047f2b7cdb5ed4696fa8f6e0
AaronTweeton <aaron@aarontweeton.com>
2023-04-27 18:33:34 -0700

    Using apiFetch to fetch latest posts.

--- src/app.jsx
+++ src/app.jsx
@@ -1,3 +1,33 @@
+import apiFetch from "@wordpress/api-fetch";
+import { useEffect, useState } from "@wordpress/element";
+
 export const App = () => {
-  return <div>This is a React component.</div>;
+  const [posts, setPosts] = useState([]);
+
+  function getPosts() {
+    apiFetch({ path: "/wp/v2/posts" }).then((data) => {
+      setPosts(data);
+    });
+  }
+
+  useEffect(() => {
+    getPosts();
+  }, []);
+
+  return (
+    <div>
+      <h2>Latest posts</h2>
+      {posts.length ? (
+        <ul>
+          {posts.map((post) => (
+            <li>
+              <a href={post.link}>{post.title.rendered}</a>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <>Fetching posts &hellip;</>
+      )}
+    </div>
+  );
 };
