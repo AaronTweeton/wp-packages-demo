561d15ad085f886caebe4fdbedfd0d1e6ea92827
AaronTweeton <aaron@aarontweeton.com>
2023-04-27 17:44:19 -0700

    Added license.

--- wp-packages-demo.php
+++ wp-packages-demo.php
@@ -3,10 +3,27 @@
 /**
  * Plugin Name: WordPress Packages Demo
  * Description: A plugin solely written to show how @wordpress packages can be used.
- * Version: 0.1.0
- * Author: Aaron Tweeton
- * Author URI: https://aarontweeton.com
+ * Version:     0.1.0
+ * Author:      Aaron Tweeton
+ * Author URI:  https://aarontweeton.com
+ * License:     GPL v2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  */
+
+/*
+WordPress Packages Demo is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+any later version.
+
+WordPress Packages Demo is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with WordPress Packages Demo. If not, see https://www.gnu.org/licenses/gpl-2.0.html.
+*/
 
 function wp_packages_demo_compose_menu_page() {
 ?>
